f44f1defca9a0bda183493f25740dad1c2bf0754
AMaupin <alexandrafink8989@gmail.com>
2025-09-24 08:03:28 -0700

    copied/paste correct data.js from Devslopes, e6.js file passed

--- data/data.js
+++ data/data.js
@@ -16,8 +16,6 @@ export const bankAccounts = [
   },
   { id: 4, name: "Candy", balance: 0.0 },
   { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
-  { id: 6, name: "Kevin", balance: 0.0 },
-  { id: 7, name: "Jon", balance: 0.0 },
 ];
 
 export const bankAccountsNoPositiveBalance = [
--- exercises/e6.js
+++ exercises/e6.js
@@ -3,35 +3,16 @@
 // Array example: bankAccounts in /data/data.js
 // getClientWithNoMoney(bankAccounts) => ['Kevin', 'Jon']
 
-export const bankAccounts = [
-  {
-    id: 1,
-    name: "Susan",
-    balance: 100.32,
-    deposits: [150, 30, 221],
-    withdrawals: [110, 70.68, 120],
-  },
-  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
-  {
-    id: 3,
-    name: "Joshua",
-    balance: 18456.57,
-    deposits: [4000, 5000, 6000, 9200, 256.57],
-    withdrawals: [1500, 1400, 1500, 1500],
-  },
-  { id: 4, name: "Candy", balance: 0.0 },
-  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
-  { id: 6, name: "Kevin", balance: 0.0 },
-  { id: 7, name: "Jon", balance: 0.0 }
-];
 
 export function getClientWithNoMoney(array) {
   let result = [];
+
   for (let i = 0; i < array.length; i++) {
     if (array[i].balance === 0) {
       result.push(array[i].name);
     }
   }
+
   return result;
 }
 
